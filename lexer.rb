70a54111025f2b5cca554722805894ab979a65ef
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-02-24 16:59:49 -0800

    Rudimentary Lexer

---
+++ lexer.rb
@@ -0,0 +1,110 @@
+class Lexer
+  attr_accessor :position, :tokens, :current_indent, :indent_stack
+  KEYWORDS = ["cell", "def", "if", "while", "true", "false", "nil"]
+
+  def initialize
+    @position = 0
+    @tokens = []
+    @current_indent = 0
+    @indent_stack = []
+  end
+
+  def tokenize code
+    code.chomp!
+
+    while @position < code.size
+      chunk = code[@position..-1]
+
+      if identifier = chunk[/\A([a-z]\w*)/, 1]
+        tokenize_identifier identifier
+      elsif constant = chunk[/\A([A-Z]\w*)/, 1]
+        tokenize_constant constant
+      elsif number = chunk[/\A([0-9]+)/, 1]
+        tokenize_number number
+      elsif string = chunk[/\A"(.*?)"/, 1]
+        tokenize_string string
+      elsif indent = chunk[/\A\n( *)/m, 1]
+        tokenize_indent indent
+      elsif operator = chunk[/\A(\|\||&&|==|!=|<=|>=)/, 1]
+        tokenize_operator operator
+      elsif chunk.match(/\A /)
+        remove_whitespace
+      else
+        tokenize_symbol chunk[0,1]
+      end
+    end
+    finalize_dedent
+
+    @tokens
+  end
+
+private
+  def tokenize_identifier identifier
+    if KEYWORDS.include?(identifier)
+      @tokens << [identifier.upcase.to_sym, identifier]
+    else
+      @tokens << [:IDENTIFIER, identifier]
+    end
+
+    @position += identifier.size
+  end
+
+  def tokenize_constant constant
+    @tokens << [:CONSTANT, constant]
+
+    @position += constant.size
+  end
+
+  def tokenize_number number
+    @tokens << [:NUMBER, number.to_i]
+
+    @position += number.size
+  end
+
+  def tokenize_string string
+    @tokens << [:STRING, string]
+
+    @position += string.size + 2
+  end
+
+  def tokenize_indent indent
+    if indent.size > @current_indent
+      @current_indent = indent.size
+      @indent_stack.push(@current_indent)
+      @tokens << [:INDENT, indent.size]
+    elsif indent.size == @current_indent
+      @tokens << [:NEWLINE, "\n"]
+    elsif indent.size < @current_indent
+      while indent.size < @current_indent
+        @indent_stack.pop
+        @current_indent = @indent_stack.last || 0
+        @tokens << [:DEDENT, indent.size]
+      end
+      @tokens << [:NEWLINE, "\n"]
+    end
+
+    @position += indent.size + 1
+  end
+
+  def tokenize_operator operator
+    @tokens << [operator, operator]
+
+    @position += operator.size
+  end
+
+  def tokenize_symbol symbol
+    @tokens << [symbol, symbol]
+
+    @position += 1
+  end
+
+  def remove_whitespace
+    @position += 1
+  end
+
+  def finalize_dedent
+    while indent = @indent_stack.pop
+      @tokens << [:DEDENT, @indent_stack.first || 0]
+    end
+  end
+end
---
+++ test/lexer_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+require "lexer"
+
+class LexerTest < Test::Unit::TestCase
+  def test_number
+    assert_equal [[:NUMBER, 1]], Lexer.new.tokenize("1")
+  end
+
+  def test_string
+    assert_equal [[:STRING, "hi"]], Lexer.new.tokenize('"hi"')
+  end
+
+  def test_identifier
+    assert_equal [[:IDENTIFIER, "name"]], Lexer.new.tokenize('name')
+  end
+
+  def test_constant
+    assert_equal [[:CONSTANT, "Name"]], Lexer.new.tokenize('Name')
+  end
+
+  def test_operator
+    assert_equal [["+", "+"]], Lexer.new.tokenize('+')
+    assert_equal [["||", "||"]], Lexer.new.tokenize('||')
+  end
+
+  def test_indent
+    code = File.open("test/code/example_1.bio", "rb").read
+    tokens = eval File.open("test/code/example_1.tokens", "rb").read
+    assert_equal tokens, Lexer.new.tokenize(code)
+  end
+end
