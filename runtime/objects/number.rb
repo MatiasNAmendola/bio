194510776e472bd2e6aaf23f4ac3e77aa799e9ef
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-03-15 19:42:19 -0700

    Added < and > to Number

--- runtime/objects/number.rb
+++ runtime/objects/number.rb
@@ -2,3 +2,13 @@ Runtime["Number"].runtime_methods["+"] = proc do |receiver,arguments|
   result = receiver.ruby_value + arguments.first.ruby_value
   Runtime["Number"].new_with_value result
 end
+
+Runtime["Number"].runtime_methods["<"] = proc do |receiver,arguments|
+  result = receiver.ruby_value < arguments.first.ruby_value
+  result ? Runtime["TrueClass"].new_with_value(true) : Runtime["FalseClass"].new_with_value(false)
+end
+
+Runtime["Number"].runtime_methods[">"] = proc do |receiver,arguments|
+  result = receiver.ruby_value > arguments.first.ruby_value
+  result ? Runtime["TrueClass"].new_with_value(true) : Runtime["FalseClass"].new_with_value(false)
+end
