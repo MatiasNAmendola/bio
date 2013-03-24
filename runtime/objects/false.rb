0f1cd712f83ae16a8b2194678aef3334b8b20c58
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-03-24 14:08:52 -0700

    Added && and || operators to true and false

---
+++ runtime/objects/false.rb
@@ -0,0 +1,17 @@
+Runtime["FalseClass"].runtime_methods["&&"] = proc do |receiver,arguments|
+  result = (receiver.ruby_value && arguments.first.ruby_value)
+  if result
+    Runtime["TrueClass"].new_with_value result
+  else
+    Runtime["FalseClass"].new_with_value result
+  end
+end
+
+Runtime["FalseClass"].runtime_methods["||"] = proc do |receiver,arguments|
+  result = (receiver.ruby_value || arguments.first.ruby_value)
+  if result
+    Runtime["TrueClass"].new_with_value result
+  else
+    Runtime["FalseClass"].new_with_value result
+  end
+end
