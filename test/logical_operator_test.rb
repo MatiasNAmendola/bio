8ead330c0ff1be897f4f1ea64f853607ab16bd25
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-03-25 21:33:06 -0700

    Added logical tests for && and ||

--- test/logical_operator_test.rb
+++ test/logical_operator_test.rb
@@ -7,8 +7,18 @@ class LogicalOperatorTest < Test::Unit::TestCase
     assert_equal false, Interpreter.new.eval("false and true").ruby_value
   end
 
+  def test_and_operator
+    assert_equal true, Interpreter.new.eval("true && true").ruby_value
+    assert_equal false, Interpreter.new.eval("false && true").ruby_value
+  end
+
   def test_or
     assert_equal true, Interpreter.new.eval("true or false").ruby_value
     assert_equal false, Interpreter.new.eval("false or false").ruby_value
+  end
+
+  def test_or_operator
+    assert_equal true, Interpreter.new.eval("true || false").ruby_value
+    assert_equal false, Interpreter.new.eval("false || false").ruby_value
   end
 end
