60396b13879bb708a50e9013c45fbd857ce30af7
Nathaniel Barnes <nathaniel.barnes@activenetwork.com>
2013-03-26 19:59:25 -0700

    Added more logical operator tests

--- test/logical_operator_test.rb
+++ test/logical_operator_test.rb
@@ -21,4 +21,31 @@ class LogicalOperatorTest < Test::Unit::TestCase
     assert_equal true, Interpreter.new.eval("true || false").ruby_value
     assert_equal false, Interpreter.new.eval("false || false").ruby_value
   end
+
+  def test_less_than
+    assert_equal true, Interpreter.new.eval("1 < 2").ruby_value
+    assert_equal false, Interpreter.new.eval("2 < 1").ruby_value
+  end
+
+  def test_less_than_or_equal
+    assert_equal true, Interpreter.new.eval("1 <= 2").ruby_value
+    assert_equal false, Interpreter.new.eval("2 <= 1").ruby_value
+    assert_equal true, Interpreter.new.eval("1 <= 1").ruby_value
+  end
+
+  def test_greater_than
+    assert_equal false, Interpreter.new.eval("1 > 2").ruby_value
+    assert_equal true, Interpreter.new.eval("2 > 1").ruby_value
+  end
+
+  def test_greater_than_or_equal
+    assert_equal false, Interpreter.new.eval("1 >= 2").ruby_value
+    assert_equal true, Interpreter.new.eval("2 >= 1").ruby_value
+    assert_equal true, Interpreter.new.eval("1 <= 1").ruby_value
+  end
+
+  def test_equals
+    assert_equal true, Interpreter.new.eval("1 == 1").ruby_value
+    assert_equal false, Interpreter.new.eval("1 == 2").ruby_value
+  end
 end
